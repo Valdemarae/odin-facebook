99bd4297e9201ba5e49fef6eacd1613253f5b494
Valdemar <valdemar.kobelis@gmail.com>
2023-10-12 13:46:38 +0300

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_104021) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_12_104320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_104021) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "information", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "description"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_information_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
