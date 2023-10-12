5d952a8de63503c03d18edb7ffb4212b2fe3b557
Valdemar <valdemar.kobelis@gmail.com>
2023-10-12 13:59:01 +0300

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :index]
+  resources :posts, except: [:show]
+  resources :friendships, only: [:index, :show]
+  root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
