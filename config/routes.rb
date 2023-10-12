84dce222a0ea3643207e3d01cad7d51e96cbab5e
Valdemar <valdemar.kobelis@gmail.com>
2023-10-12 15:01:55 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index]
   resources :posts, except: [:show]
   resources :friendships, only: [:index, :show]
+  resources :likes, :comments, only: [:create]
   root 'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
