acf1ea1a37e177825543c90999ba6af7852f202e
Valdemar <valdemar.kobelis@gmail.com>
2023-10-12 13:28:32 +0300

    Create Friendship model with associations and migrate db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :friendships
+
+  def friends
+    ids1 = Friendship.where(user_id: id, confirmed: true).pluck(:another_user_id)
+    ids2 = Friendship.where(another_user_id: id, confirmed: true).pluck(:user_id)
+    ids = ids1 + ids2
+    User.where(id: ids)
+  end
+
+  def sent_invitations
+    ids = friendships.where(confirmed: false).pluck(:another_user_id)
+    User.where(id: ids)
+  end
+
+  def received_invitations
+    ids = Friendship.where(another_user_id: id, confirmed: false).pluck(:user_id)
+    User.where(id: ids)
+  end
 end
